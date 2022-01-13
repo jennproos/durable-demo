24c066173f1e9687b99bd9cdadef3fb13d784341
Jenn Proos <Jenn.Proos@meijer.com>
2022-01-13 10:50:30 -0500

    Add README and another orchestration that follows the Fan Out Fan In pattern

---
+++ FanOutFanInOrchestration.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace Meijer.Function
+{
+    public static class FanOutFanInOrchestration
+    {
+        [FunctionName("FanOutFanInOrchestration")]
+        public static async Task<string> RunOrchestrator(
+            [OrchestrationTrigger] IDurableOrchestrationContext context)
+        {
+            var parallelTasks = new List<Task<int>>();
+
+            // Get a list of N work items to process in parallel.
+            int[] workBatch = await context.CallActivityAsync<int[]>("F1", 5);
+            for (int i = 0; i < workBatch.Length; i++)
+            {
+                Task<int> task = context.CallActivityAsync<int>("F2", workBatch[i]);
+                parallelTasks.Add(task);
+            }
+
+            var taskResults = await Task.WhenAll(parallelTasks);
+
+            // Aggregate all N outputs and send the result to F3
+            int sum = 0;
+            foreach(int result in taskResults)
+            {
+                sum += result;
+            }
+
+            return await context.CallActivityAsync<string>("F3", sum);
+        }
+
+        [FunctionName("F1")]
+        public static int[] F1_CreateWorkBatch([ActivityTrigger] int count, ILogger log)
+        {
+            log.LogInformation("Inside F1");
+            var rand = new Random();
+
+            int[] workBatch = new int[count];
+            for (int i = 0; i < count; i ++)
+            {
+                // Get a random integer from 1 to 100
+                workBatch[i] = rand.Next(1, 100);
+            }
+
+            return workBatch;
+        }
+
+        [FunctionName("F2")]
+        public static int F2_MultiplyByTwo([ActivityTrigger] int input, ILogger log)
+        {
+            log.LogInformation($"Inside F2 with input {input}");
+            return input*2;
+        }
+
+        [FunctionName("F3")]
+        public static string F3_ReportSum([ActivityTrigger] int sum, ILogger log)
+        {
+            log.LogInformation($"Inside F3 with input {sum}");
+            return $"The result of this run is: {sum}!";
+        }
+
+        [FunctionName("FanOutFanInOrchestration_HttpStart")]
+        public static async Task<HttpResponseMessage> HttpStart(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestMessage req,
+            [DurableClient] IDurableOrchestrationClient starter,
+            ILogger log)
+        {
+            // Function input comes from the request content.
+            string instanceId = await starter.StartNewAsync("FanOutFanInOrchestration", null);
+
+            log.LogInformation($"Started orchestration with ID = '{instanceId}'.");
+
+            return starter.CreateCheckStatusResponse(req, instanceId);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# durable-demo
+# Durable Functions Demo
+
+Durable functions are really freaking cool. Currently this repo has 2 very simple durable orchestrations in it that follow the first two [application patterns](https://docs.microsoft.com/en-us/azure/azure-functions/durable/durable-functions-overview?tabs=csharp#application-patterns) listed in Microsoft's documentation.
+
+For a full overview of Azure Durable Functions, go to [Microsoft's documentation](https://docs.microsoft.com/en-us/azure/azure-functions/durable/) for more info and examples.
+
+## Installation
+
+This project is built on the .NET Core 3.1 Framework, so if you don't have that installed you can get it from the [download page](https://dotnet.microsoft.com/en-us/download/dotnet/3.1).
+
+Durable Functions rely on an Azure storage account to store the state of an orchestration. For this demo I just used my local storage, and in order to do that on your machine you will need to install [azurite](https://docs.microsoft.com/en-us/azure/storage/common/storage-use-azurite?tabs=visual-studio#install-azurite).
+
+## Usage
+
+Open the project in Visual Studio Code and open a terminal in the root directory. Run `dotnet build`. In a separate terminal, simply run `azurite` to boot up your local storage. Finally, start debugging the project by either clicking `Run` then `Start Debugging` or pressing `F5`
+
+It may take a couple seconds to start but eventually you should see something like this in your terminal indicating that the project is running:
+
+```powershell
+Function Runtime Version: 4.0.1.16815
+
+[2022-01-13T15:39:15.385Z] Found C:\Users\3087099\Documents\durable-demo\durable-demo.csproj. Using for user secrets file configuration.
+
+Functions:
+
+        FanOutFanInOrchestration_HttpStart: [GET,POST] http://localhost:7071/api/FanOutFanInOrchestration_HttpStart
+
+        VeryCoolOrchestration_HttpStart: [GET,POST] http://localhost:7071/api/VeryCoolOrchestration_HttpStart
+
+        F1: activityTrigger
+
+        F2: activityTrigger
+
+        F3: activityTrigger
+
+        FanOutFanInOrchestration: orchestrationTrigger
+
+        VeryCoolOrchestration: orchestrationTrigger
+
+        VeryCoolOrchestration_Hello: activityTrigger
+
+For detailed output, run func with --verbose flag.
+[2022-01-13T15:39:23.475Z] Host lock lease acquired by instance ID '000000000000000000000000C97C1807'.  
+```
+
+If you see an error message saying `Failed to verify "AzureWebJobsStore" connection specified in "local.settings.json"`, make sure you run `azurite` in a separate terminal before starting the debugger.
+
+Both orchestrations can be started by `ctrl + click`ing on the links listed in the terminal after starting up the project. Set some breakpoints and check out the terminal logs to see the true ✨magic✨ of durable functions!
